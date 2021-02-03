5db7a1e7c8dd2d215ddcde2e2346618f46d6b73d
NickFlexer <nickkomarov.job@gmail.com>
2021-02-03 17:06:09 +0300

    add setting table

--- bot/models.py
+++ bot/models.py
@@ -9,3 +9,11 @@ class Task(models.Model):
     def __str__(self):
         return self.summary
 
+
+class Setting(models.Model):
+    topic = models.CharField(max_length=200)
+    setting = models.CharField(max_length=200)
+    value = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.topic + "-" + self.setting
